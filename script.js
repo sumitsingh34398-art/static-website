0c194e3871fe340a0c241ea8eb10ce828ec0d53a
sumitsingh34398-art <sumitsingh34398@gmail.com>
2026-06-11 22:52:14 +0530

    Add theme toggle and form submission functionality

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log("hello");
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+
+themeBtn.innerHTML = "☀️ Light Mode";
+
+}else{
+
+themeBtn.innerHTML = "🌙 Dark Mode";
+
+}
+
+});
+
+const contactForm = document.querySelector(".contact-form");
+
+contactForm.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert(
+"Thank You! Your message has been submitted successfully."
+);
+
+contactForm.reset();
+
+});
+
+window.addEventListener("load", () => {
+
+console.log("Cloud Dashboard Loaded Successfully");
+
+});
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+card.addEventListener("mouseenter", () => {
+
+card.style.transform = "translateY(-8px)";
+
+});
+
+card.addEventListener("mouseleave", () => {
+
+card.style.transform = "translateY(0px)";
+
+});
+
+});
