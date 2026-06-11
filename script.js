339a8606f64b0a7a82b2826a79a57e3f08d35f7a
sumitsingh34398-art <sumitsingh34398@gmail.com>
2026-06-12 01:50:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ contactForm.addEventListener("submit", function(e) {
         console.log(error);
     });
 });
+// Dark Mode Toggle
+
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        darkModeBtn.innerHTML = "☀ Light Mode";
+    } else {
+        darkModeBtn.innerHTML = "🌙 Dark Mode";
+    }
+});
