af881e90be0c0c5f2654e15c42e354680a40a896
sumitsingh34398-art <sumitsingh34398@gmail.com>
2026-06-11 23:19:44 +0530

    Integrate emailjs for contact form and remove dark mode
    Removed dark mode toggle and added emailjs integration for contact form submission.

--- script.js
+++ script.js
@@ -1,55 +1,21 @@
-const themeBtn = document.getElementById("themeBtn");
+emailjs.init("SpLKfuA_nR1mfy8lUh");
 
-themeBtn.addEventListener("click", () => {
+const contactForm = document.getElementById("contactForm");
 
-document.body.classList.toggle("dark");
+contactForm.addEventListener("submit", function(e) {
+    e.preventDefault();
 
-if(document.body.classList.contains("dark")){
-
-themeBtn.innerHTML = "☀️ Light Mode";
-
-}else{
-
-themeBtn.innerHTML = "🌙 Dark Mode";
-
-}
-
-});
-
-const contactForm = document.querySelector(".contact-form");
-
-contactForm.addEventListener("submit", function(e){
-
-e.preventDefault();
-
-alert(
-"Thank You! Your message has been submitted successfully."
-);
-
-contactForm.reset();
-
-});
-
-window.addEventListener("load", () => {
-
-console.log("Cloud Dashboard Loaded Successfully");
-
-});
-
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-
-card.addEventListener("mouseenter", () => {
-
-card.style.transform = "translateY(-8px)";
-
-});
-
-card.addEventListener("mouseleave", () => {
-
-card.style.transform = "translateY(0px)";
-
-});
-
+    emailjs.sendForm(
+        "service_7xf3h6a",
+        "template_xsqpw6o",
+        this
+    )
+    .then(function() {
+        alert("Message Sent Successfully!");
+        contactForm.reset();
+    })
+    .catch(function(error) {
+        alert("Failed to send message!");
+        console.log(error);
+    });
 });
